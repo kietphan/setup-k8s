a63203efa6989e57d99dc4bcc29721b5a74c665d
Kiet Phan <kiet@iwa.fi>
2020-02-19 09:21:26 +0700

    update command

--- create_instance.sh
+++ create_instance.sh
@@ -1,3 +1,3 @@
 gcloud compute instances create k8s-master \
       k8s-worker1 k8s-worker2 --zone asia-southeast1-b --machine-type g1-small --image-family ubuntu-1804-lts \
---image-project ubuntu-os-cloud --metadata-from-file ssh-keys=/Users/kietphan/.ssh/id_rsa.pub
+--image-project ubuntu-os-cloud --metadata-from-file ssh-keys=/Users/kietphan/.ssh/google_compute_engine.pub
